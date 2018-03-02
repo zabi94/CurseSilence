048631caa9285c82c9245ce045ad1c90c94027bd
zabi94 <zabi94@gmail.com>
2018-03-02 20:55:10 +0100

    Moar mc versions

--- src/main/java/zabi/minecraft/cursesilence/CurseSilence.java
+++ src/main/java/zabi/minecraft/cursesilence/CurseSilence.java
@@ -10,12 +10,14 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(name=CurseSilence.MOD_NAME, modid = CurseSilence.MOD_ID, version=CurseSilence.MOD_VERSION, clientSideOnly=true)
+@Mod(name=CurseSilence.MOD_NAME, modid = CurseSilence.MOD_ID, version=CurseSilence.MOD_VERSION, clientSideOnly=true, acceptedMinecraftVersions=CurseSilence.MC_VERSIONS)
 public class CurseSilence {
 	
 	public static final String MOD_ID = "cursesilence";
 	public static final String MOD_NAME = "Curse Silence";
 	public static final String MOD_VERSION = "1.0";
+	public static final String MC_VERSIONS = "[1.11,1.12.2)";
+	
 	
 	public static final ArrayList<Enchantment> curses = new ArrayList<Enchantment>();
 	
